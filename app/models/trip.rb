bfeed0089df9f1f80ac6dd4423006140e6056a8b
eefreddy <emmanuelfreddy@gmail.com>
2018-01-28 10:26:26 -0700

    app/models/trip.rb

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -4,8 +4,8 @@ class Trip < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
 
   geocoded_by :start_location, :latitude  => :start_latitude, :longitude => :start_longitude
-  after_validation :geocode, :if => :start_location_changed?
 
-  geocoded_by :destination, :latitude  => :end_latitude, :longitude => :end_longitude
-  after_validation :geocode, :if => :destination_changed?
+  after_validation :geocode
+  # geocoded_by :destination, :latitude  => :end_latitude, :longitude => :end_longitude
+  # after_validation :geocode
 end
